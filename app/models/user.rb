339f4adfc4b4434893ddec1ad0948bbf6c3d4ac0
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2016-04-10 01:04:08 -0400

    adds role field in the User form and validates it

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validate :role_name
+
+  def self.available_roles
+    ['admin', 'subject']
+  end
+
+  private
+    def role_name
+      if not User.available_roles().include?(role)
+        errors.add :base, "Role #{role} does not exist in the system."
+        return false
+      end
+    end
 end
