0eeaaacdc3042204cceff9a422faed7fcc19d531
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2016-04-10 00:18:40 -0400

    makes sign-in home page for unsigned users and static_pages#home for signed users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
-  root to: "static_pages#home", via: [:get]
-
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'static_pages#home', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   scope '/admin' do
     resources :users
   end
