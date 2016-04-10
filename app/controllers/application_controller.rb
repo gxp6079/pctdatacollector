0cef1c4a1bfb969877bf3f38141d2824fa4b20b5
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2016-04-10 16:01:08 -0400

    fixes cancan ForbiddenAttributeError with rails 4 (generates params with namespace_resource)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Fix cancan ForbiddenError with rails 4 (generate params with namespace_resource)
+  before_filter do
+    resource = controller_path.singularize.gsub('/', '_').to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.html { redirect_to root_url, :alert => exception.message }
