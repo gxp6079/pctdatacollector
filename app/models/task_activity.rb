13c7f7dfcdaee0800298ea30effe2261792a6c9f
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2016-04-10 20:50:11 -0400

    adds task_activities_take page

---
+++ app/models/task_activity.rb
@@ -0,0 +1,8 @@
+class TaskActivity
+  attr_reader :system, :task, :files
+  def initialize user
+    # @system = randomly_select_an_unfinished_system
+    # @task = randomly_select_an_unfinished_task_from @system
+    # @files = Files.all.where("system_id = ?" @system.id)
+  end
+end
