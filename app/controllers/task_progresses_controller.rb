5dcd0dc0d03fbf4cb6ff7fccdc7f004cf010d9b8
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2016-04-12 15:45:22 -0400

    adds authorization to task_progresses

--- app/controllers/task_progresses_controller.rb
+++ app/controllers/task_progresses_controller.rb
@@ -1,4 +1,7 @@
 class TaskProgressesController < ApplicationController
+
+  load_and_authorize_resource
+
   before_action :set_task_progress, only: [:show, :edit, :update, :destroy]
 
   # GET /task_progresses
