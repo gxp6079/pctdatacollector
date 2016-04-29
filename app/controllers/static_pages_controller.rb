996ce867069426903f081eadd6c8b2f7f854c23a
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2016-04-29 18:42:58 -0400

    adds tasks answer sheet download link

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,32 @@ class StaticPagesController < ApplicationController
   def home
     @static_pages = StaticPage.new("Wellcome")
   end
+
+  def download_tasks
+    if current_user.group == ".k"
+      filename = "trygve_tasks.docx"
+    elsif current_user.group == ".java"
+      filename = "java_tasks.docx"
+    elsif
+      respond_to do |format|
+        format.html { redirect_to root_url, notice: 'You are not in a group for the experiment.' }
+        format.json { head :no_content }
+      end
+      return
+    end
+
+    begin
+      send_file(
+        "#{Rails.root}/public/task_documents/#{filename}",
+        filename: filename,
+        type: "application/docx"
+      )
+    rescue ActionController::MissingFile => e
+      respond_to do |format|
+        format.html { redirect_to root_url, notice: 'The answer sheet file is not available.' }
+        format.json { head :no_content }
+      end
+    end
+
+  end
 end
