7ddd55eb38d38b7045b7f04e37b3120afbc74c88
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2016-05-23 14:57:44 -0400

    subjects can answer only one pre-test

--- app/controllers/pre_test_activities_controller.rb
+++ app/controllers/pre_test_activities_controller.rb
@@ -3,12 +3,21 @@ class PreTestActivitiesController < ApplicationController
   load_and_authorize_resource
 
   def take
-    @pre_test = PreTest.where("pre_tests.group = ?", current_user.group).first
+    if current_user.pre_test_answers.size <= 0
+      @pre_test = PreTest.where("pre_tests.group = ?", current_user.group).first
+    else
+      respond_to do |format|
+        format.html { redirect_to :root, notice: 'You have already answered the Pre test.' }
+        format.json { head :no_content }
+      end
+    end
   end
 
   def finish
-    current_user.pre_test_answers.create(pre_test_id: params['pre_test_id'], answer: "{\"answer\" : #{params['answer']}}")
-    current_user.update_attribute(:is_in_training, false)
-    render js: "window.location = '#{task_activities_system_description_url}'"
+    if current_user.pre_test_answers.size <= 0
+      current_user.pre_test_answers.create(pre_test_id: params['pre_test_id'], answer: "{\"answer\" : #{params['answer']}}")
+      current_user.update_attribute(:is_in_training, false)
+      render js: "window.location = '#{task_activities_system_description_url}'"
+    end
   end
 end
